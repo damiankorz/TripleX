5487027c9881709f17c1511ea3a7aab644eff306
Damian Korzeniewski <damian.korz@gmail.com>
2019-05-15 15:46:38 -0500

    Add intro

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+void PrintIntro()
+{
+    std::cout << "You are a clandestine operative tasked with hacking a secured mainframe\n";
+    std::cout << "You must enter the correct codes to continue...\n";
+}
+
 int main()
 {
+    PrintIntro();
     return 0;
 }
