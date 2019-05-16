2e750ca5edca9c44b0cac0089e26661766eb7cf9
Damian Korzeniewski <damian.korz@gmail.com>
2019-05-16 13:59:06 -0500

    Add conditional check for guess, add ASCII art to print intro

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,11 +2,23 @@
 
 void PrintIntro()
 {
-    std::cout << "You are a clandestine operative tasked with hacking a secure mainframe...\n";
-    std::cout << "You must enter the correct codes to continue...\n";
+    std::cout << " ___      ___ ___      ___ ___    ____ _________ ___        ___    ____          ____\n";
+    std::cout << "|   \\    |   |   |    |   |   |  /   /|         |   \\      /   |   \\   \\        /   /\n";
+    std::cout << "|    \\   |   |   |    |   |   | /   / |   ______|    \\    /    |    \\   \\      /   /\n";
+    std::cout << "|     \\  |   |   |    |   |   |/   /  |  |      |     \\  /     |     \\   \\    /   /\n";
+    std::cout << "|      \\ |   |   |    |   |       /   |  |______|      \\/      |      \\   \\  /   / \n";
+    std::cout << "|       \\|   |   |    |   |      /    |         |              |       \\   \\/   /\n";
+    std::cout << "|   |\\       |   |    |   |      \\    |   ______|   |\\    /|   |       /        \\\n";
+    std::cout << "|   | \\      |   |    |   |       \\   |  |      |   | \\  / |   |      /    /\\    \\\n";
+    std::cout << "|   |  \\     |   |____|   |   |\\   \\  |  |______|   |  \\/  |   |     /    /  \\    \\\n";
+    std::cout << "|   |   \\    |            |   | \\   \\ |         |   |      |   |    /    /    \\    \\\n";
+    std::cout << "|___|    \\___|____________|___|  \\___\\|_________|___|      |___|   /____/      \\____\\\n\n";
+
+    std::cout << "You are a clandestine operative tasked with disarming a nuclear warhead...\n";
+    std::cout << "You must bypass each security layer by entering the correct codes...\n\n";
 }
 
-int main()
+void PlayGame()
 {
     const int CodeA = 4;
     const int CodeB = 3;
@@ -17,17 +29,29 @@ int main()
 
     PrintIntro();
 
-    std::cout << "\n+ There are 3 numbers in the code\n";
+    std::cout << "+ There are 3 numbers in the code\n";
     std::cout << "+ The code adds up to " << CodeSum << "\n";
-    std::cout << "+ The product of the code is " << CodeProduct << "\n";
+    std::cout << "+ The product of the code is " << CodeProduct << "\n\n";
 
     int GuessA, GuessB, GuessC;
-
-    std::cout << "Enter the code:\n";
     std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
+
+    if(GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "You win!\n";
+    }
+    else 
+    {
+        std::cout << "Incorrect code entered\n";
+    }
+}
+
+int main()
+{
+    PlayGame();
 
     return 0;
 }
